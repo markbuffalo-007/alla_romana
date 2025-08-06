58eeec85f7365cfe19a339b1f6698ba13fe1a1c4
markred2631 <markred2631@gmail.com>
2025-08-06 10:56:30 -0700

    add 404 page

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from app.database import get_bucket
 from app.web.gradient import gradients
@@ -28,15 +29,19 @@ app.mount(
 # Context
 # -------------------------------------------------
 
-def _context(request: Request, bucket: str, payer: str):
-    expenses, net, settlements = get_bucket(bucket)
-
+def _base_context(request: Request):
     return {
         "request": request,
         "settings": {
             "title": "Alla Romana",
             "background": gradients('standard')
-        },
+        }
+    }
+
+def _context(request: Request, bucket: str, payer: str):
+    expenses, net, settlements = get_bucket(bucket)
+
+    result = {
         "bucket": bucket,
         "expenses": expenses,
         "net": net,
@@ -44,6 +49,8 @@ def _context(request: Request, bucket: str, payer: str):
         "current_user": payer,
         "icon_for": icon_for,
     }
+    result |= _base_context(request)
+    return result
 
 # -------------------------------------------------
 # Routes
@@ -58,3 +65,10 @@ async def balances(request: Request, tab: str, bucket: str, payer: str):
         return templates.TemplateResponse("/fragments/expenses.html", _context(request, bucket, payer))
     if tab == 'balances':
         return templates.TemplateResponse("/fragments/balances.html", _context(request, bucket, payer))
+
+@app.exception_handler(StarletteHTTPException)
+async def custom_404_handler(request: Request, exc: StarletteHTTPException):
+    if exc.status_code == 404:
+        return templates.TemplateResponse('error.html', _base_context(request))
+    # Let FastAPI handle other HTTP exceptions
+    raise exc
